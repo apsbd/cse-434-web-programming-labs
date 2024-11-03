00aaf35ff2bc0b0170de2fd25d57d5ed2fc6d310
Mohiuddin Ahmed <mohiuddin_ahmed_07@yahoo.com>
2024-11-03 21:37:43 +0600

    Update index.php

---
+++ lab/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Users Registration</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .container {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Simple users registration</h1>
+        <a href="register.php">Register</a>
+    </div>
+</body>
+</html>
